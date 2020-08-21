4a7046fad66506942547f00992f3a1bc4866d61e
Kwonhyukjoon <victorehko12@naver.com>
2020-08-21 16:27:23 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config/config.env" });
+// const fileupload = require("express-fileupload");
+// const path = require("path");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.json());
+// app.use(fileupload());
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.use(morgan("combined"));
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res, next) => {
+  res.json({ success: true });
+});
+
+app.listen(PORT, () => {
+  console.log("App listening on port 3000!");
+});
